c6d6ae07cd7ff0c6917d0082cbaa94c7c3235701
Александр Заринский <100155701+besapuz@users.noreply.github.com>
2023-04-03 16:32:46 +0300

    Create cheat_sheet.py

---
+++ cheat_sheet.py
@@ -0,0 +1,115 @@
+#Создаем коннект:
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+  
+#Передаем переменные:
+connection = create_connection(
+    "postgres", "postgres", "abc123", "127.0.0.1", "5432"
+)
+
+
+#Создаем дефолтную БД sm_app:
+def create_database(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+create_database_query = "CREATE DATABASE sm_app"
+create_database(connection, create_database_query)
+Подключаемся к БД 
+connection = create_connection(
+    "sm_app", "postgres", "abc123", "127.0.0.1", "5432"
+)
+
+
+#Создам функцию обработки таблиц:
+def execute_query(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+Создаем таблицу:
+Здесь для указания столбцов с автоматическим инкрементом используется ключевое слово SERIAL
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL, 
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+)
+"""
+
+execute_query(connection, create_users_table)
+
+
+#Создаем таблицу со вторичным ключем:
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id SERIAL PRIMARY KEY, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER REFERENCES users(id)
+)
+"""
+
+execute_query(connection, create_posts_table)
+
+
+#Передаем записи в таблицу:
+users = [
+    ("James", 25, "male", "USA"),
+    ("Leila", 32, "female", "France"),
+    ("Brigitte", 35, "female", "England"),
+    ("Mike", 40, "male", "Denmark"),
+    ("Elizabeth", 21, "female", "Canada"),
+]
+
+user_records = ", ".join(["%s"] * len(users))
+
+insert_query = (
+    f"INSERT INTO users (name, age, gender, nationality) VALUES {user_records}"
+)
+
+connection.autocommit = True
+
+
+#Делаем запрос по значению:
+select_post_description = "SELECT description FROM posts WHERE id = 2"
+
+post_description = execute_read_query(connection, select_post_description)
+
+
+#Делаем запрос JOIN:
+select_users_posts = """
+SELECT
+  users.id,
+  users.name,
+  posts.description
+FROM
+  posts
+  INNER JOIN users ON users.id = posts.user_id
+"""
+
+users_posts = execute_read_query(connection, select_users_posts)
